19005c74d2e296d1746e25d8c277d8f1a238579c
geek10010 <36638017+geek10010@users.noreply.github.com>
2018-03-15 23:54:03 +0300

    src/index.js function solveSudoku

--- src/index.js
+++ src/index.js
@@ -1,4 +1,67 @@
 module.exports = function solveSudoku(matrix) {
- let veryStrangeResult = matrix.map(r => [...r.map(i => i === 0 ? [] : i)]);
- return veryStrangeResult;
+
+  function findEmpty(matrix) {
+    for (let rownum = 0; rownum < 9; rownum++) {
+      for (let colnum = 0; colnum < 9; colnum++) {
+        if (matrix[rownum][colnum] === 0) {
+          return [rownum, colnum];
+        }
+      }
+    }
+    return [];
+  }
+  
+  function presentInRow(matrix, candidatNumber, currentCell) {
+    for (let i = 0; i < 9; i++) {
+      if (candidatNumber === matrix[currentCell[0]][i]) return true;
+    }
+    return false;
+  }
+
+  function presentInCol(matrix, candidatNumber, currentCell) {
+    for (let i = 0; i < 9; i++) {
+      if (candidatNumber === matrix[i][currentCell[1]]) return true;
+    }
+    return false;
+  }
+  
+  function presentInBlock(matrix, candidatNumber, currentCell) {
+    row = Math.floor(currentCell[0] - currentCell[0] % 3);
+    col = Math.floor(currentCell[1] - currentCell[1] % 3);
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (candidatNumber === matrix[row + i][col + i]) return true;
+      }
+    }
+    return false;
+  }
+  
+  function isAllowed(matrix, candidatNumber, currentCell) {
+    if (!presentInRow(matrix, candidatNumber, currentCell) && !presentInCol(matrix, candidatNumber, currentCell) && !presentInBlock(matrix, candidatNumber, currentCell)) {
+      return true;
+    }
+    return false;
+  }
+
+
+  function solving(matrix) {
+    let currentCell = findEmpty(matrix);
+    
+    if (currentCell.length === 0) return true;
+    
+    for (let number = 1; number <= 9; number++) {
+      if (isAllowed(matrix, number, currentCell)) {
+        matrix[currentCell[0]][currentCell[1]] = number;
+        
+        if (solving(matrix)) return true; 
+        
+        matrix[currentCell[0]][currentCell[1]] = 0;
+      }
+    }
+    return false;
+  }
+
+  if (solving(matrix)) {
+    return matrix;
+  }
 }
