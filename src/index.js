30a1baaefb6ba4e5681e9a7a328f6a099053aef5
Dzmitry Tarasau <36638017+geek10010@users.noreply.github.com>
2018-09-14 13:41:03 +0300

    src/index.js fix function solveSudoku

--- src/index.js
+++ src/index.js
@@ -1,67 +1,86 @@
 module.exports = function solveSudoku(matrix) {
+  const sudoku = matrix;
 
-  function findEmpty(matrix) {
-    for (let rownum = 0; rownum < 9; rownum++) {
-      for (let colnum = 0; colnum < 9; colnum++) {
-        if (matrix[rownum][colnum] === 0) {
+  function findEmptyCell(grid) {
+    for (let rownum = 0; rownum < 9; rownum += 1) {
+      for (let colnum = 0; colnum < 9; colnum += 1) {
+        if (grid[rownum][colnum] === 0) {
           return [rownum, colnum];
         }
       }
     }
     return [];
   }
-  
-  function presentInRow(matrix, candidatNumber, currentCell) {
-    for (let i = 0; i < 9; i++) {
-      if (candidatNumber === matrix[currentCell[0]][i]) return true;
+
+
+  function isPresentInRow(candidatNumber, rownum) {
+    return sudoku[rownum].includes(candidatNumber);
+  }
+
+
+  function isPresentInCol(candidatNumber, colnum) {
+    for (let i = 0; i < 9; i += 1) {
+      if (candidatNumber === sudoku[i][colnum]) {
+        return true;
+      }
     }
+
     return false;
   }
 
-  function presentInCol(matrix, candidatNumber, currentCell) {
-    for (let i = 0; i < 9; i++) {
-      if (candidatNumber === matrix[i][currentCell[1]]) return true;
-    }
-    return false;
-  }
-  
-  function presentInBlock(matrix, candidatNumber, currentCell) {
-    row = Math.floor(currentCell[0] - currentCell[0] % 3);
-    col = Math.floor(currentCell[1] - currentCell[1] % 3);
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (candidatNumber === matrix[row + i][col + i]) return true;
+
+  function isPresentInBlock(candidatNumber, cell) {
+    const [rownum, colnum] = cell;
+    const row = Math.floor(rownum - (rownum % 3));
+    const col = Math.floor(colnum - (colnum % 3));
+    let blockRow;
+
+    for (let i = 0; i < 3; i += 1) {
+      blockRow = sudoku[row + i].slice(col, col + 3);
+      if (blockRow.includes(candidatNumber)) {
+        return true;
       }
     }
+
     return false;
   }
-  
-  function isAllowed(matrix, candidatNumber, currentCell) {
-    if (!presentInRow(matrix, candidatNumber, currentCell) && !presentInCol(matrix, candidatNumber, currentCell) && !presentInBlock(matrix, candidatNumber, currentCell)) {
+
+
+  function isAllowed(candidatNumber, cell) {
+    const [rownum, colnum] = cell;
+    if (!isPresentInRow(candidatNumber, rownum)
+      && !isPresentInCol(candidatNumber, colnum)
+      && !isPresentInBlock(candidatNumber, cell)) {
       return true;
     }
     return false;
   }
 
 
-  function solving(matrix) {
-    let currentCell = findEmpty(matrix);
-    
-    if (currentCell.length === 0) return true;
-    
-    for (let number = 1; number <= 9; number++) {
-      if (isAllowed(matrix, number, currentCell)) {
-        matrix[currentCell[0]][currentCell[1]] = number;
-        
-        if (solving(matrix)) return true; 
-        
-        matrix[currentCell[0]][currentCell[1]] = 0;
+  function solving(grid) {
+    const currentCell = findEmptyCell(sudoku);
+    const [rownum, colnum] = currentCell;
+
+    if (currentCell.length === 0) {
+      return true;
+    }
+
+    for (let number = 1; number <= 9; number += 1) {
+      if (isAllowed(number, currentCell)) {
+        sudoku[rownum][colnum] = number;
+
+        if (solving(grid)) {
+          return true;
+        }
+
+        sudoku[rownum][colnum] = 0;
       }
     }
+
     return false;
   }
 
-  if (solving(matrix)) {
-    return matrix;
-  }
-}
+  solving(sudoku);
+
+  return sudoku;
+};
